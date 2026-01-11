5451ce9f13791969a4f6a2ae2fa7839457b17996
bouajilaprog <bouajilamedyessine@gmail.com>
2026-01-11 01:14:24 +0100

    feat: add compile function to convert generated typst into pdf

--- src/compiler/compile.ts
+++ src/compiler/compile.ts
@@ -1,4 +1,5 @@
 import { execa } from "execa";
+import { paths } from "../utils/path";
 
 export async function verifyEnv() {
   try {
@@ -7,6 +8,20 @@ export async function verifyEnv() {
     throw new Error(
       "Typst not found. Install it from https://typst.app/docs/install/"
     );
+  }
+}
+
+
+export async function compileToPdf(inputFile: string, outputFile: string) {
+  try {
+    await execa("typst", [
+      "compile",
+      "--root", paths.root,
+      inputFile,
+      outputFile,
+    ]);
+  } catch (err) {
+    throw new Error("Compilation failed. " + (err as Error).message);
   }
 }
 
